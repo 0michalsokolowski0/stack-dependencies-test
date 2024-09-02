3c761ae0fcc3f1008dbd592d29619369a6797ccb
Michal Sokolowski <0michalsokolowski0@gmail.com>
2024-09-02 14:30:37 +0200

    A change

--- stack-a/main.tf
+++ stack-a/main.tf
@@ -3,6 +3,11 @@ resource "random_string" "db_connection_string" {
   special = false
 }
 
+resource "random_string" "some_random_string" {
+  length  = 16
+  special = false
+}
+
 output "DB_CONNECTION_STRING" {
   value = "postgres://${random_string.db_connection_string.result}@localhost:5432/mydb"
 }
