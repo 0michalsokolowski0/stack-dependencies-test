a5e7a79c9e326411b71dc7515f47fe677b310b85
Michal Sokolowski <0michalsokolowski0@gmail.com>
2025-01-08 13:29:46 +0100

    Some changes

--- stack-b/main.tf
+++ stack-b/main.tf
@@ -2,3 +2,10 @@ data "spacelift_stack" "stack-a-data" {
   stack_id = "dependencies-stack-a"
 }
 
+output "stack-a-id" {
+  value = data.spacelift_stack.stack-a-data.id
+}
+
+output "stack-a-administrative" {
+  value = data.spacelift_stack.stack-a-data.administrative
+}
