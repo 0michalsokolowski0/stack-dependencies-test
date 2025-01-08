67400c54a2e22e0a250f1b62c625ddf75dd519a2
Michal Sokolowski <0michalsokolowski0@gmail.com>
2025-01-08 13:51:00 +0100

    Some changes

--- stack-b/main.tf
+++ stack-b/main.tf
@@ -14,7 +14,7 @@ data "terraform_remote_state" "example_stack_state" {
   backend = "remote"
 
   config = {
-    hostname     = "spacelift.io"
+    hostname     = "spacelift.tf"
     organization = "0michalsokolowski0"
     workspaces = {
       name = "dependencies-stack-a"
