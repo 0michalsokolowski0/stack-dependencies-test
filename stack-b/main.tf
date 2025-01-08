5b40e81b53299de3f2d7c079a3255d9bf3f88a63
Michal Sokolowski <0michalsokolowski0@gmail.com>
2025-01-08 12:59:41 +0100

    Some changes

--- stack-b/main.tf
+++ stack-b/main.tf
@@ -1,15 +1,3 @@
-variable "APP_DB_URL" {
-  description = "The database URL for the application"
-  type        = string
-  default = "postgres://user:password@localhost:5432/mydb"
-}
-
-resource "null_resource" "app" {
-  provisioner "local-exec" {
-    command = "echo ${var.APP_DB_URL} > app_db_url.txt"
-  }
-}
-
 data "terraform_remote_state" "stack_a" {
   backend = "remote"
   config = {
